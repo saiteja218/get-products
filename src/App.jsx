6c7cfed6c156b038f8c69d970e94deb74bef2d64
saiteja218 <saitejareddy218@gmail.com>
2024-09-30 20:10:53 +0530

    error resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import GetData from './GetData';
 
@@ -11,7 +9,7 @@ function App() {
    <div>
  <GetData/>
    </div>
-  )
+  )   
 }
 
 export default App
