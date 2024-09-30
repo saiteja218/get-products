c597c61fc1fd19177a29d90cf6f55ff5c4cff0d8
saiteja218 <saitejareddy218@gmail.com>
2024-09-30 19:31:33 +0530

    first commit

---
+++ src/getData.jsx
@@ -0,0 +1,14 @@
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// export default function getData() {
+//     const [products,getProducts]=useState([]);
+
+//     useEffect()
+
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
