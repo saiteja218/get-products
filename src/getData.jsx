2a0e7e4500987130bcb1cd1673ff80f7e44a4850
saiteja218 <saitejareddy218@gmail.com>
2024-09-30 20:08:07 +0530

    third commit

--- src/getData.jsx
+++ src/getData.jsx
@@ -17,7 +17,7 @@ export default function GetData() {
             getProducts(data);
         }
         getData()
-    },[])
+    },[]) 
 
   return (
     <div style={{display:"grid", gridTemplateColumns:"1fr 1fr 1fr"}}>
